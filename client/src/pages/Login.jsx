356feaa10cd1b318bc6dd77efef1a5fbacf99ff5
Victor Manakana <vtmanakana@gmail.com>
2024-04-05 16:08:05 +0200

    redux toolkit for user

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  logInStart,
+  logInSuccess,
+  logInFailure,
+} from '../redux/user/userSlice';
 import OAuth from '../components/OAuth';
 
 export default function Login() {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const handleChange = (e) => {
     setFormData({
       ...formData,
@@ -16,7 +22,7 @@ export default function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true);
+      dispatch(logInStart());
       const res = await fetch('/api/auth/login', {
         method: 'POST',
         headers: {
@@ -27,23 +33,19 @@ export default function Login() {
       const data = await res.json();
       console.log(data);
       if (data.success === false) {
-        setLoading(false);
-        setError(data.message);
+        dispatch(logInFailure(data.message));
         return;
       }
-      setLoading(false);
-      setError(null);
+      dispatch(logInSuccess(data));
       navigate('/');
     } catch (error) {
-      setLoading(false);
-      setError(error.message);
+      dispatch(logInFailure(error.message));
     }
   };
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Log In</h1>
       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
-        
         <input
           type='email'
           placeholder='email'
@@ -63,14 +65,14 @@ export default function Login() {
           disabled={loading}
           className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
         >
-          {loading ? 'Loading...' : 'Log in'}
+          {loading ? 'Loading...' : 'Login In'}
         </button>
         <OAuth/>
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Dont have an account?</p>
         <Link to={'/sign-up'}>
-          <span className='text-blue-700'>Sign Up</span>
+          <span className='text-blue-700'>Sign up</span>
         </Link>
       </div>
       {error && <p className='text-red-500 mt-5'>{error}</p>}
