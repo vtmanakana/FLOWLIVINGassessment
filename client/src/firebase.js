8c96877d0e430df08d3f3e37ebaf244d32b22c90
Victor Manakana <vtmanakana@gmail.com>
2024-04-05 16:31:05 +0200

    firebase for google auth make sure

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: ,
+  authDomain: "proper-t-view.firebaseapp.com",
+  projectId: "proper-t-view",
+  storageBucket: "proper-t-view.appspot.com",
+  messagingSenderId: "932391946537",
+  appId: "1:932391946537:web:0f451140dde702d1e60f38"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
