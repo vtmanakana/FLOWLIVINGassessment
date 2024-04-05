a3a40baaa0e6766acaec45e1331ecb8e29bbfa6b
Victor Manakana <vtmanakana@gmail.com>
2024-04-05 15:40:12 +0200

    login make sure

--- api/routes/auth.routes.js
+++ api/routes/auth.routes.js
@@ -4,7 +4,7 @@ import { google, signOut, signin, signup } from '../controllers/auth.controller.
 const router = express.Router();
 
 router.post("/signup", signup);
-router.post("/signin", signin);
+router.post("/login", login);
 router.post('/google', google);
 router.get('/signout', signOut)
 
