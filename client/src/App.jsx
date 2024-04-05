f1b11f37ccc551f75aa3caa5e430529add7d3377
Victor Manakana <vtmanakana@gmail.com>
2024-04-05 12:00:38 +0200

    server config fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,13 +4,15 @@ import SignUp from './pages/SignUp';
 import About from './pages/About';
 import Profile from './pages/Profile';
 import Login from './pages/Login';
+//import Header from './components/Header';
 
 
 export default function App() {
   return (
     <BrowserRouter>
+    
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/home' element={<Home />} />
         <Route path='/log-in' element={<Login />} />
         <Route path='/sign-up' element={<SignUp />} />
         <Route path='/about' element={<About />} />
