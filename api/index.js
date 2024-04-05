22dbbdf13a46e3cbbb65bf4d04f05b38eb6b4275
Victor Manakana <vtmanakana@gmail.com>
2024-04-05 15:03:20 +0200

    mongo data fill

--- api/index.js
+++ api/index.js
@@ -1,19 +1,51 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.routes.js';
+import authRouter from './routes/auth.routes.js';
+import listingRouter from './routes/listing.routes.js';
+import cookieParser from 'cookie-parser';
+import path from 'path';
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(() => {
-    console.log('MongoDB Connected Successfully');
-}).catch((err) => {
-    console.error(err);
-})  
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
+  const __dirname = path.resolve();
 
 const app = express();
 
-app.listen(4700, () =>
-{
-    console.log('Server is running on port 4700!!!');
-}
-);
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.listen(4700, () => {
+  console.log('Server is running on port 4700!');
+});
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
+
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
