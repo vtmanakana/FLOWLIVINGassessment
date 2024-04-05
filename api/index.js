27eb4981cbbadc790688bcbfb7c799e3e707b400
Victor Manakana <vtmanakana@gmail.com>
2024-04-05 13:18:59 +0200

    mongoDB Connection

--- api/index.js
+++ api/index.js
@@ -1,9 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('MongoDB Connected Successfully');
+}).catch((err) => {
+    console.error(err);
+})  
+
 
 const app = express();
 
-app.listen(3000, () =>
+app.listen(4700, () =>
 {
-    console.log('Server is running on port 3000!!!');
+    console.log('Server is running on port 4700!!!');
 }
 );
