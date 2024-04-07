2d20a4f7735475a956bb9ed0695d70169b9f04f0
Victor Manakana <vtmanakana@gmail.com>
2024-04-07 22:25:08 +0200

    api

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -64,7 +64,7 @@ export default function Header() {
                 alt='profile'
               />
             ) : (
-              <li className=' text-slate-700 hover:underline'> Sign in</li>
+              <li className=' text-slate-700 hover:underline'> Log in</li>
             )}
           </Link>
         </ul>
